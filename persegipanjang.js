b501b85a39d069ef8b4b970bae5a701a07c3734f
risamena <hannytuhuteru@gmail.com>
2024-02-03 23:18:43 +0900

    penambahan rumus persegi panjang

---
+++ persegipanjang.js
@@ -0,0 +1,18 @@
+// Jangan sentuh code disini
+function TentukanLuasDanKelilingPersegiPanjang(p, l) {
+    const panjang = p;
+    const lebar = l;
+    const luas = panjang * lebar;
+    const keliling = 2 * (panjang + lebar);
+    // TODO: buat logic dibawah sini dimana panjangnya 5 cm dan lebarnyanya 7 cm
+    console.log("Panjang Persegi Panjang:", panjang, "cm");
+    console.log("Lebar Persegi Panjang:", lebar, "cm");
+    console.log("Luas Persegi Panjang:", luas, "cm");
+    console.log("Keliling Persegi Panjang:", keliling, "cm");
+    // TODO: buatlah logic untuk menghitung luas dan keliling dibawah
+    // TODO: gunakan console.log untuk mencetak hasil luas dan keliling
+    
+    // Akhir pembuatan code
+  }
+  
+  TentukanLuasDanKelilingPersegiPanjang(5, 7);
